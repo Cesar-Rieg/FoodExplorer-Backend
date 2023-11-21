8ae18b1bec4a193822d89b34c50c014e4d64df15
César Rieg <cesar@hiper.com.br>
2023-11-21 20:45:48 -0300

    CM - Arquivo knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,17 @@
 const path = require("path");
+const DatabaseConstants = require("./src/Database/DatabaseConstants.js");
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "Database", DatabaseConstants.DatabaseName)
     },
     pool: {
       afterCreate: (conn, callback) => conn.run("PRAGMA foreign_keys = ON", callback)
     },
     migrations: {
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+      directory: path.resolve(__dirname, "src", "Database", "Knex", "Migrations")
     },
     useNullAsDefault: true
   }
